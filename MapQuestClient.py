fe3882f093b6157b0b8ba4d52fa478666c540bba
phanjamie <phanjamie33@gmail.com>
2022-09-18 16:31:10 -0700

    add URL and KEY

--- MapQuestClient.py
+++ MapQuestClient.py
@@ -1,3 +1,7 @@
 from urllib import request
 from dotenv import load_dotenv
-print("Hello World")
+import os
+
+load_dotenv()
+MAPQUEST_KEY = os.getenv("MAPQUEST_KEY")
+MAPQUEST_URL = os.getenv("MAPQUEST_URL")
